3d2dee45bc836f80c390d31a15fa57205435a952
amael-ls <amael.le-squin@ign.fr>
2026-09-01 17:33:50 +0200

    Paths and other common variables

---
+++ code/zenodo/global_variables.R
@@ -0,0 +1,8 @@
+#### Aim of scripts: Contains global variables used in other scripts
+## Paths
+path_data = "./data/"
+path_output = "./results/"
+path_models = "./stan_models/"
+
+## Variables for running models
+n_chains = 4
