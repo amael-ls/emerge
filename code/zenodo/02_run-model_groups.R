fe855f8b668df54eabcd307659af8401f5378313
amael-ls <amael.le-squin@ign.fr>
2026-09-01 17:40:21 +0200

    Script to run the pooled species models

---
+++ code/zenodo/02_run-model_groups.R
@@ -0,0 +1,136 @@
+#### Aim of script: Run the models for pooled species
+## Comments
+# This file is to run the pooled species models
+
+## Packages needed to reproduce the study
+renv::restore()
+
+library(data.table)
+library(cmdstanr)
+library(stringi)
+
+## Load data
+# Tool functions
+source("./tool_functions.R")
+
+# Global variables (paths and others)
+source("./global_variables.R")
+
+# Tree data (14 species)
+tree_dt = readRDS(paste0(path_data, "tree_dt.rds"))
+setkey(tree_dt, group)
+
+# Seeds that were used to run the models (full and submodel)
+seed_dt = readRDS(paste0(path_data, "ls_seeds.rds"))
+
+# Load stan models
+fullmodel = cmdstan_model(paste0(path_models, "fullmodel.stan"))
+submodel = cmdstan_model(paste0(path_models, "submodel.stan"))
+
+
+
+# --------------------------------------------------------------------------------------
+# --------------------    Run the full model, for pooled species    --------------------
+# --------------------------------------------------------------------------------------
+
+success_dt = tree_dt[, .(N_indiv = .N), by = group]
+success_dt[, rhat_ok := NA]
+success_dt[, no_div := NA]
+
+setkey(success_dt, group)
+
+ls_groups = success_dt[, group]
+
+## Run model
+for (gr in ls_groups)
+{
+	print(paste("Running", gr))
+
+	# Subset to targeted group for stanData
+	stanData = list(
+		N = tree_dt[.(gr)][, .N],
+		bole_volume_m3 = tree_dt[.(gr)][, bole_volume_m3],
+		total_volume_m3 = tree_dt[.(gr)][, total_volume_m3]
+	)
+
+	## Run model
+	if (file.exists(paste0(path_data, gr, "_fullmodel.rds")))
+	{
+		fit = readRDS(paste0(path_data, gr, "_fullmodel.rds"))
+	} else {
+		fit = fullmodel$sample(data = stanData, chains = n_chains, parallel_chains = min(n_chains, 4),
+			iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10)
+
+		## Save results
+		fit$save_output_files(dir = path_data, basename = gr, random = FALSE)
+		saveRDS(fit, paste0(path_data, gr, "_fullmodel.rds"))
+	}
+
+	div = plot_gr(fit, gr, simplif = FALSE, n_bins = 4, pal = "Hiroshige", selected_variable = "height",
+		print_plot = TRUE)
+
+	rhat_ok = max(div[["rhats"]]) <= 1.01
+	no_div = !div[["any_div"]]
+	
+	success_dt[.(gr), rhat_ok := ..rhat_ok]
+	success_dt[.(gr), no_div := ..no_div]
+
+	if (div[["any_div"]])
+		warning(paste("Divergences for", gr))
+}
+
+success_dt[, success_full := rhat_ok & no_div]
+
+
+
+# ---------------------------------------------------------------------------------------
+# ---------------------    Run the sub model, for pooled species    ---------------------
+# ---------------------------------------------------------------------------------------
+
+ls_pb = success_dt[(!success_full), group]
+
+success_dt[, rhat_ok_sub := NA]
+success_dt[, no_div_sub := NA]
+
+## Run model
+for (gr in ls_pb)
+{
+	print(paste("Running", gr))
+
+	# Subset to targeted group for stanData
+	stanData = list(
+		N = tree_dt[.(gr)][, .N],
+		bole_volume_m3 = tree_dt[.(gr)][, bole_volume_m3],
+		total_volume_m3 = tree_dt[.(gr)][, total_volume_m3]
+	)
+
+	## Run model
+	if (file.exists(paste0(path_data, gr, "_submodel.rds")))
+	{
+		fit = readRDS(paste0(path_data, gr, "_submodel.rds"))
+	} else {
+		fit = submodel$sample(data = stanData, chains = n_chains, parallel_chains = min(n_chains, 4),
+			iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10)
+
+		## Save results
+		fit$save_output_files(dir = path_data, basename = paste0(gr, "_submodel"), random = FALSE)
+		saveRDS(fit, paste0(path_data, gr, "_submodel.rds"))
+	}
+
+	div = plot_gr(fit, gr, simplif = TRUE, n_bins = 4, pal = "Hiroshige", selected_variable = "height",
+		print_plot = TRUE)
+
+	rhat_ok = max(div[["rhats"]]) <= 1.01
+	no_div = !div[["any_div"]]
+	
+	success_dt[.(gr), rhat_ok_sub := ..rhat_ok]
+	success_dt[.(gr), no_div_sub := ..no_div]
+
+	if (div[["any_div"]])
+		warning(paste("Divergences for", gr))
+}
+
+success_dt[, success_sub := rhat_ok_sub & no_div_sub]
+
+if (!file.exists(paste0(path_output, "group-success.rds")))
+	saveRDS(success_dt, paste0(path_output, "group-success.rds"))
