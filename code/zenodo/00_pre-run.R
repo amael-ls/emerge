5b8d546411b466bde282341905b4143e012e8391
amael-ls <amael.le-squin@ign.fr>
2026-09-01 17:33:35 +0200

    Added renv and then many stuff (computed r, merged ls-groups, moved path, ...)

--- code/zenodo/00_pre-run.R
+++ code/zenodo/00_pre-run.R
@@ -1,39 +1,105 @@
 #### Aim of script: Make directories, check data
-## Packages
+## Comments
+# For cmdstanr, you might need to run (see https://mc-stan.org/cmdstanr/articles/cmdstanr.html):
+# install.packages("cmdstanr", repos = c('https://stan-dev.r-universe.dev', getOption("repos")))
+
+## Packages needed to reproduce the study
+renv::restore()
+
 library(data.table)
+library(cmdstanr)
+library(stringi)
 
 ## Check/make directories
-path_data = "./data/"
-path_output = "./results/"
+source("./global_variables.R")
 
 if (!dir.exists(path_data))
-	stop(paste0("The folder <", path_data, ">is missing. Please check your working directory"))
+	stop(paste0("The folder <", path_data, "> is missing. Check path and working directory"))
+
+if (!dir.exists(path_models))
+	stop(paste0("The folder <", path_models, "> is missing. Check path and working directory"))
 
 if (!dir.exists(path_output))
 	dir.create(path_output)
 
 ## Check data
-if (!file.exists("tree_dt.rds"))
+tree_file = paste0(path_data, "tree_dt.rds")
+if (!file.exists(tree_file))
 {
-	# Loading Emerge data
-	inra = readRDS(paste0(path_data, "inra.rds"))
+	# Loading Inra data (Oudin's protocol)
+	filename = paste0(path_data, "inra.rds")
+	if (!file.exists(filename))
+		stop(paste0("The file <", filename, "> does not exist! Check path and working directory"))
+	inra = readRDS(filename)
 	inra = inra[, .(speciesName_sci, tree_id = unique_id, plot_id, fct_type, year, circumference_m, height,
 		taper_height = taper_height_flo, bole_volume_m3, total_volume_m3)]
 	setkey(inra, speciesName_sci)
 	
-	# Loading Swiss data
-	swiss = readRDS(paste0(mnt_point, "data/switzerland.rds"))
+	# Loading Swiss data (EFM protocol)
+	filename = paste0(path_data, "switzerland.rds")
+	if (!file.exists(filename))
+		stop(paste0("The file <", filename, "> does not exist! Check path and working directory"))
+	swiss = readRDS(filename)
 	swiss = unique(swiss[, .(speciesName_sci, tree_id, plot_id, fct_type, year, circumference_m, height,
 		taper_height = hdec, bole_volume_m3, total_volume_m3)])
 	setkey(swiss, speciesName_sci)
 	
-	## Modern Emerge data
-	emerge = readRDS(paste0(mnt_point, "data/emerge_2009-2010.rds"))
+	# Modern Emerge data (Oudin's protocol, data collected in 2 campaigns 2009-2010)
+	filename = paste0(path_data, "emerge_2009-2010.rds")
+	if (!file.exists(filename))
+		stop(paste0("The file <", filename, "> does not exist! Check path and working directory"))
+	emerge = readRDS(filename)
 	emerge[, year := as.integer(stringi::stri_replace_all(str = dataset, replacement = "", regex = "emerge_"))]
 	emerge = emerge[, .(speciesName_sci, tree_id, fct_type, year, circumference_m, height, taper_height,
 		bole_volume_m3 = bole_volume_conic_m3, total_volume_m3)]
 	
-	## Bind everything
+	# Species groups, determined during EMERGE project
+	filename = paste0(path_data, "ls-groups.rds")
+	if (!file.exists(filename))
+		stop(paste0("The file <", filename, "> does not exist! Check path and working directory"))
+	ls_groups = readRDS(filename)
+	
+	# Bind everything
 	tree_dt = rbindlist(list(inra = inra, swiss = swiss, emerge = emerge), idcol = "origin", fill = TRUE)
-
+	tree_dt[, r := bole_volume_m3/total_volume_m3]
+	tree_dt = merge.data.table(x = tree_dt, y = ls_groups, by = "speciesName_sci", all.x = TRUE)
+	tree_dt[, any(is.na(group))]
+	saveRDS(tree_dt, tree_file)
+} else {
+	tree_dt = readRDS(tree_file)
 }
+
+## Keep only species with more than 150 individual records
+ls_species = tree_dt[, .(n_indiv = .N), by = speciesName_sci][order(-n_indiv)]
+
+min_indiv = 150
+ls_species = ls_species[n_indiv > min_indiv]
+
+## Subset tree_dt and save the 14 species dataset
+tree_dt = tree_dt[speciesName_sci %in% ls_species[, speciesName_sci]]
+setkey(tree_dt, speciesName_sci, plot_id)
+
+filename = paste0(path_data, "tree_dt_14species.rds")
+if (!file.exists(filename))
+	saveRDS(tree_dt, filename)
+
+## Check stan installation, help can be found at https://mc-stan.org/cmdstanr/index.html
+cmdstanr::cmdstan_path()
+if (cmdstanr::cmdstan_path() == "")
+	warning("You might have to install Stan language or to set the correct path")
+# cmdstanr::check_cmdstan_toolchain()
+# cmdstanr::install_cmdstan(cores = 2)
+# cmdstanr::cmdstan_version() # I used the version 2.39.0
+
+## Compile models
+# Full model
+filename = paste0(path_models, "fullmodel.stan")
+if (!file.exists(filename))
+	stop(paste0("The model <", filename, "> could not be found"))
+fullmodel = cmdstan_model(filename)
+
+# Submodel
+filename = paste0(path_models, "submodel.stan")
+if (!file.exists(filename))
+	stop(paste0("The model <", filename, "> could not be found"))
+submodel = cmdstan_model(filename)
