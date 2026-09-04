818c322b5c07c2cfd6e910245bde4a3c08c3b507
amael-ls <amael.le-squin@ign.fr>
2026-09-04 09:54:47 +0200

    Tracking file for pgfplots output

---
+++ code/zenodo/07_pgfplots.R
@@ -0,0 +1,4 @@
+#### Aim of script: Generate the files for pgfplots table Latex
+## Comments:
+# This file is not necessary for the study itself. It only creates csv files
+#	to be read by latex using pgfplots and pgfplotstable
