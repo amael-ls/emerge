d1665824f1f44cbc373520b2333d2250f7fbc243
amael-ls <amael.le-squin@ign.fr>
2026-09-04 12:19:42 +0200

    Started script

---
+++ code/zenodo/06_VEF-longuetaud.R
@@ -0,0 +1,7 @@
+#### Aim of script: Compute averaged VEF from Longuetaud et al.
+## Comments:
+# Bibliography
+#	Longuetaud, Fleur, Philippe Santenoise, Frédéric Mothe, Tristan Senga Kiessé, Michaël Rivoire,
+#		Laurent Saint-André, Nina Ognouabi, and Christine Deleuze. 2013.
+#		Modeling Volume Expansion Factors for Temperate Tree Species in France.
+#		Forest Ecology and Management 292: 111–21. https://doi.org/10.1016/j.foreco.2012.12.023.
