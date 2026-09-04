5219434b68a638353a4584a87c56614a821293c1
amael-ls <amael.le-squin@ign.fr>
2026-09-04 12:04:45 +0200

    Minor modifications, moved group fit to 02_fit-groups.R

--- code/zenodo/01_run-model.R
+++ code/zenodo/01_run-model.R
@@ -40,15 +40,12 @@ for (sp in ls_species)
 {
 	print(paste("Running", sp))
 
-	filename = paste0(stri_replace(str = sp, regex = " ", replacement = "-"), "_fullmodel")
+	filename = paste0(stri_replace(str = sp, regex = " ", replacement = "-"), "_fullmodel_theta")
+	le_cid = seed_dt[.(sp), full]
 
 	if (sp == "Quercus sp.")
-	{
 		filename = stri_replace(str = filename, regex = "\\.", replacement = "")
-		sp = ls_species[stri_detect(str = ls_species, regex = "Quercus")]
-		warning("For quercus sp., I also add all the other Quercus")
-	}
-	
+
 	# Subset to targeted species for stanData
 	stanData = list(
 		N = tree_dt[.(sp)][, .N],
@@ -62,7 +59,7 @@ for (sp in ls_species)
 		fit = readRDS(paste0(path_output, filename, ".rds"))
 	} else {
 		fit = fullmodel$sample(data = stanData, chains = n_chains,
-			parallel_chains = min(n_chains, 4), seed = seed_dt[.(sp), full],
+			parallel_chains = min(n_chains, 4), seed = le_cid,
 			iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10)
 
 		# Save results
@@ -72,6 +69,7 @@ for (sp in ls_species)
 
 	div = plot_sp(fit, sp, simplif = FALSE, n_bins = 4, pal = "Hiroshige",
 		selected_variable = "height", print_plot = TRUE)
+	plot_divergences(fit, div$loc)
 
 	if (div[["any_div"]])
 		warning(paste("Divergences for", sp))
@@ -88,14 +86,11 @@ for (sp in ls_species)
 	print(paste("Running", sp))
 
 	filename = paste0(stri_replace(str = sp, regex = " ", replacement = "-"), "_submodel")
+	le_cid = seed_dt[.(sp), submodel]
 
 	if (sp == "Quercus sp.")
-	{
 		filename = stri_replace(str = filename, regex = "\\.", replacement = "")
-		sp = ls_species[stri_detect(str = ls_species, regex = "Quercus")]
-		warning("For quercus sp., I also add all the other Quercus")
-	}
-	
+
 	# Subset to targeted species for stanData
 	stanData = list(
 		N = tree_dt[.(sp)][, .N],
@@ -109,7 +104,7 @@ for (sp in ls_species)
 		fit = readRDS(paste0(path_output, filename, ".rds"))
 	} else {
 		fit = submodel$sample(data = stanData, chains = n_chains,
-			parallel_chains = min(n_chains, 4), seed = seed_dt[.(sp), submodel],
+			parallel_chains = min(n_chains, 4), seed = le_cid,
 			iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10)
 
 		# Save results
@@ -119,65 +114,8 @@ for (sp in ls_species)
 
 	div = plot_sp(fit, sp, simplif = TRUE, n_bins = 4, pal = "Hiroshige",
 		selected_variable = "height", print_plot = TRUE)
+	plot_joint_dirty(fit, div$loc)
 
 	if (div[["any_div"]])
 		warning(paste("Divergences for", sp))
-}
-
-
-
-# ----------------------------------------------------------------------------------------
-# ------------------    Run the full model, for broadleaves/conifers    ------------------
-# ----------------------------------------------------------------------------------------
-
-## Run model for broadleaves
-filename = "broadleaf"
-
-# Subset to broadleaves
-broadleaves = c("Fagus sylvatica", "Fraxinus excelsior", "Quercus petraea", "Quercus sp.")
-all(broadleaves %in% tree_dt[, unique(speciesName_sci)])
-
-stanData = list(
-	N = tree_dt[.(broadleaves)][, .N],
-	bole_volume_m3 = tree_dt[.(broadleaves)][, bole_volume_m3],
-	total_volume_m3 = tree_dt[.(broadleaves)][, total_volume_m3]
-)
-
-# Run model
-if (!file.exists(paste0(path_output, filename, ".rds")))
-{
-	fit = fullmodel$sample(data = stanData, chains = n_chains, parallel_chains = min(n_chains, 4),
-		iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10, seed = seed_dt["broadleaf", full])
-
-	## Save results
-	fit$save_output_files(dir = path_output, basename = filename, random = FALSE)
-	saveRDS(fit, paste0(path_output, filename, ".rds"))
-}
-
-
-
-## Run model for coniefers
-filename = "conifer"
-
-# Subset to conifers
-conifers = tree_dt[, unique(speciesName_sci)]
-conifers = conifers[!(conifers %in% broadleaves)]
-
-length(conifers) + length(broadleaves) == length(tree_dt[, unique(speciesName_sci)])
-
-stanData = list(
-	N = tree_dt[.(conifers)][, .N],
-	bole_volume_m3 = tree_dt[.(conifers)][, bole_volume_m3],
-	total_volume_m3 = tree_dt[.(conifers)][, total_volume_m3]
-)
-
-# Run model
-if (!file.exists(paste0(path_output, filename, ".rds")))
-{
-	fit = fullmodel$sample(data = stanData, chains = n_chains, parallel_chains = min(n_chains, 4),
-		iter_warmup = 1500, iter_sampling = 1000, max_treedepth = 10, seed = seed_dt["broadleaf", full])
-
-	## Save results
-	fit$save_output_files(dir = path_output, basename = filename, random = FALSE)
-	saveRDS(fit, paste0(path_output, filename, ".rds"))
 }
