0f4186927182ccb58a52ec7b21ac820db87c61ee
amael-ls <amael.le-squin@ign.fr>
2026-09-10 10:43:44 +0200

    Giving up renv + moved longuetaud's table to global_varaibles.R

--- code/zenodo/06_VEF-longuetaud.R
+++ code/zenodo/06_VEF-longuetaud.R
@@ -7,8 +7,6 @@
 #		Forest Ecology and Management 292: 111–21. https://doi.org/10.1016/j.foreco.2012.12.023.
 
 #### Packages needed to reproduce the study
-renv::restore()
-
 library(data.table)
 library(cmdstanr)
 library(stringi)
@@ -70,28 +68,6 @@ genus = tree_dt[, as.list(lapply(.SD, quantile, probs = c(0.025, 0.975))), by = 
 genus[, vars := rep(c("q025", "q975"), .N/2)]
 
 genus = dcast.data.table(data = genus, formula = genus ~ vars, value.var = c("dbh_cm", "height"))
-
-## Parameters and range, verified
-longuetaud_pars = data.table(
-	genus = c("betula", "carpinus", "fagus", "fraxinus", "quercus",
-		"abies", "larix", "picea", "pinus", "pseudotsuga"),
-	b1 = rep(6.83, 10),
-	b2 = c(1.016, 1.133, 0.842, 1.112, 1.320,
-		1.496, 0.966, 0.953, 0.566, 0.744),
-	b3 = rep(1.009, 10),
-	b4 = c(0.448, 0.822, 0.627, 0.470, 0.245,
-		0.566, 0.454, 0.473, 0.336, 0.783),
-	min_d = c(12, 8, 7, 8, 7,
-		7, 9, 8, 7, 7),
-	max_d = c(31, 38, 79, 43, 102,
-		90, 42, 67, 71, 34),
-	min_h = c(14.7, 11.3, 5.5, 11.5, 6,
-		6.9, 9, 7.7, 5.1, 7),
-	max_h = c(28.5, 30, 42.5, 26.7, 40,
-		37.8, 25.6, 41.3, 33.4, 29.5),
-	is_broadleaf = rep(c(TRUE, FALSE), each = 5),
-	key = "genus"
-)
 
 longuetaud_pars = merge.data.table(longuetaud_pars, genus, by = "genus")
 
