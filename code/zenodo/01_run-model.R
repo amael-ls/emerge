ec7e29ecd7fdef39f9bf7fa7a00a9ad3600574bb
amael-ls <amael.le-squin@ign.fr>
2026-09-10 10:39:18 +0200

    Giving up renv

--- code/zenodo/01_run-model.R
+++ code/zenodo/01_run-model.R
@@ -6,8 +6,6 @@
 # For a quick test, use sp = "Fraxinus excelsior", it is the fastest to run
 
 ## Packages needed to reproduce the study
-renv::restore()
-
 library(data.table)
 library(cmdstanr)
 library(stringi)
@@ -119,3 +117,108 @@ for (sp in ls_species)
 	if (div[["any_div"]])
 		warning(paste("Divergences for", sp))
 }
+
+
+
+#### DRAFT ZONE --------------------------------------------------------------------------------
+# Extract posterior draws
+draws_df = setDT(posterior::as_draws_df(fit$draws(variables = c("c", "j", "k", "m", "n", "s", "tau", "theta"))))
+setnames(draws_df, old = c(".chain", ".iteration", ".draw"), new = c("chain", "iteration", "draw"))
+
+colours = MetBrewer::met.brewer("Egypt")[1:4]
+
+plot(1:1000, draws_df[1:1000, tau], col = colours[1], type = "l", ylim = c(0, 2))
+lines(1001:2000 - 1000, draws_df[1001:2000, tau], col = colours[2])
+lines(2001:3000 - 2000, draws_df[2001:3000, tau], col = colours[3])
+lines(3001:4000 - 3000, draws_df[3001:4000, tau], col = colours[4])
+legend("topright", legend = paste("Chain", 1:4), fill = colours)
+
+plot(1:1000, draws_df[1:1000, j], col = colours[1], type = "l")
+lines(1001:2000 - 1000, draws_df[1001:2000, j], col = colours[2])
+lines(2001:3000 - 2000, draws_df[2001:3000, j], col = colours[3])
+lines(3001:4000 - 3000, draws_df[3001:4000, j], col = colours[4])
+legend("topright", legend = paste("Chain", 1:4), fill = colours)
+
+mean_params = draws_df[, lapply(.SD, FUN = mean), .SDcols = c("c", "j", "k", "m", "n", "s", "tau"), by = chain]
+
+r_func = function(x, pars)
+{
+	return((pars[, m] - pars[, c]) * exp(pars[, j] - pars[, k]*x) * (pars[, k]*x/pars[, j])^pars[, j] +
+		pars[, c] - (pars[, c] - pars[, n])*exp(-pars[, s]*x));
+}
+
+curve(r_func(x, mean_params[chain == 1, .(c, j, k, m, n, s)]), col = colours[1], to = 8)
+curve(r_func(x, mean_params[chain == 2, .(c, j, k, m, n, s)]), col = colours[2], add = TRUE)
+curve(r_func(x, mean_params[chain == 3, .(c, j, k, m, n, s)]), col = colours[3], add = TRUE)
+curve(r_func(x, mean_params[chain == 4, .(c, j, k, m, n, s)]), col = colours[4], add = TRUE)
+legend("topright", legend = paste("Chain", 1:4), fill = colours)
+
+plot(draws_df$c, draws_df$j, xlab = "c", ylab = "j", pch = 16, cex = 0.4)
+f = function(x, linMod)
+{
+	qq = coefficients(linMod)
+	return(qq[1] + qq[2]*x + qq[3]*x^2)
+}
+lin = lm(formula = j ~ 1 + c + I(c^2), data = draws_df)
+curve(f(x, lin), add = TRUE, lwd = 2, col = "#CD212A")
+
+# 1. Take draws at both ends of the c-j banana
+lim_c = draws_df[, quantile(c, prob = c(0.025, 0.975))]
+dt = draws_df[c < lim_c["2.5%"] | c > lim_c["97.5%"]]
+
+plot(dt$c, dt$j, xlab = "c", ylab = "j", pch = 16, cex = 0.4)
+curve(f(x, lin), add = TRUE, lwd = 2, col = "#CD212A")
+
+curve(r_func(x, dt[1, .(c, j, k, m, n, s)]), to = 8)
+for (i in seq_len(dt[, .N]))
+{
+	if (dt[i, c] < lim_c["2.5%"])
+		curve(r_func(x, dt[i, .(c, j, k, m, n, s)]), add = TRUE, col = "#FAB255")
+	if (dt[i, c] > lim_c["97.5%"])
+		curve(r_func(x, dt[i, .(c, j, k, m, n, s)]), add = TRUE, col = "#0F7BA2")
+}
+
+## Check mu_1
+mu_1 = function(x, pars)
+	return((pars[, m] - pars[, c]) * exp(pars[, j] - pars[, k]*x) * (pars[, k]*x/pars[, j])^pars[, j])
+
+mu_1_alt = function(x, pars)
+	return((pars[, m] - pars[, c]) * exp(pars[, j] * (1 - x/pars[, tau])) * (x/pars[, tau])^pars[, j])
+
+curve(mu_1_alt(x, dt[1, .(c, j, k, m, n, s, tau)]), to = 8, ylim = c(0, 0.2))
+for (i in seq_len(dt[, .N]))
+{
+	if (dt[i, c] < lim_c["2.5%"])
+		curve(mu_1_alt(x, dt[i, .(c, j, k, m, n, s, tau)]), add = TRUE, col = "#FAB255")
+	if (dt[i, c] > lim_c["97.5%"])
+		curve(mu_1_alt(x, dt[i, .(c, j, k, m, n, s, tau)]), add = TRUE, col = "#0F7BA2")
+}
+legend("topright", legend = c("low", "high"), fill = c("#FAB255", "#0F7BA2"))
+
+
+fun = function(x, j)
+	return(exp(j*(1 - x)))
+
+DL_0 = function(x, j)
+	return(exp(j) - j*exp(j)*x + j^2*exp(j)/2*x^2)
+
+curve(fun(x, 0.75), from = -0.75, to = 0.57)
+curve(DL_0(x, 0.75), add = TRUE, col = "#CD212A", lty = "dashed")
+
+#### END DRAFT ZONE --------------------------------------------------------------------------------
+
+
+
+################################################
+#### Stuff to find a prior
+Ex = 1.2
+Vx = 3
+
+sigma = sqrt(log(Vx/Ex^2 + 1))
+mu = log(Ex) - 0.5*sigma^2
+
+sim = rlnorm(1e5, mu, sigma)
+mean(sim)
+var(sim)
+
+curve(dlnorm(x, mu, sigma), to = 5)
