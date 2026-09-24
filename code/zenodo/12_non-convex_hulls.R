bec0276358eb6e0dd79b6de0f031f842e025ccca
amael-ls <amael.le-squin@ign.fr>
2026-09-24 13:55:17 +0200

    Tracking script for overlap space training vs NFI datasets

---
+++ code/zenodo/12_non-convex_hulls.R
@@ -0,0 +1,8 @@
+#### Aim of script: Compute a non-convex hull in the c-h space and check overlaps NFI vs fitting data
+## Comments
+# I expect the bole volumes of both datasets (NFI and training_dt) to overlap quite well,
+#	while it might not be the case for the circumference -- height space range
+#
+
+#### Load packages
+library(data.table)
